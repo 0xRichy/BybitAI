f9bbd116f654e48a5cc20a6ee34aa57fe92a1e5d
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-07-29 01:23:30 +0200

    Update and rename BybitAI.py to main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from DataFetcher import DataFetcher
+from IndicatorGenerator import IndicatorGenerator
+from MachineLearningModel import MachineLearningModel
+from OrderPlacer import OrderPlacer
+import ccxt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Your main function here

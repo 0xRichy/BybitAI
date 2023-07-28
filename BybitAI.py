2ae1c3f8bd98527f6f6eff50bac23ea02324efb5
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-07-29 00:51:49 +0200

    Update BybitAI.py

--- BybitAI.py
+++ BybitAI.py
@@ -27,122 +27,131 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.addHandler(logging.StreamHandler(telegram_bot.send_message))
 
-# Define function to fetch real-time market data
-def fetch_real_time_data(symbol, timeframe):
-    try:
-        ohlcv = bybit_exchange.fetch_ohlcv(symbol, timeframe, limit=1)
-        return np.array([candle[4] for candle in ohlcv])
-    except Exception as e:
-        logger.error(f"Error fetching data: {e}")
-        return None
+class DataFetcher:
+    def __init__(self, exchange):
+        self.exchange = exchange
 
-# Define function to generate AI indicator
-def generate_ai_indicator(data):
-    # Your AI indicator generation code goes here
-    # For example, let's calculate the Exponential Moving Average (EMA)
-    ema_period = 12
-    ema = np.mean(data[-ema_period:])
-    return ema
+    def fetch_real_time_data(self, symbol, timeframe):
+        try:
+            ohlcv = self.exchange.fetch_ohlcv(symbol, timeframe, limit=1)
+            return np.array([candle[4] for candle in ohlcv])
+        except Exception as e:
+            logger.error(f"Error fetching data: {e}")
+            return None
 
-# Define function to create machine learning dataset
-def create_dataset(data, window_size):
-    X, y = [], []
-    for i in range(len(data) - window_size - 1):
-        window = data[i:i+window_size]
-        X.append(window)
-        y.append(1 if data[i+window_size] > generate_ai_indicator(window) else -1)
-    return np.array(X), np.array(y)
+class IndicatorGenerator:
+    def generate_ai_indicator(self, data):
+        ema_period = 12
+        ema = np.mean(data[-ema_period:])
+        return ema
 
-# Define function to create machine learning dataset
-def create_dataset(data, window_size):
-    X, y = [], []
-    for i in range(len(data) - window_size - 1):
-        window = data[i:i+window_size]
-        X.append(window)
-        y.append(1 if data[i+window_size] > generate_ai_indicator(window) else -1)
-    return np.array(X), np.array(y)
+class MachineLearningModel:
+    def __init__(self):
+        self.classifier = RandomForestClassifier()
 
-# Define function to place a market order with detailed logging
-def place_market_order(symbol, side, quantity, leverage):
-    try:
-        order = bybit_exchange.create_market_order(symbol, side, quantity, {'leverage': leverage})
+    def create_dataset(self, data, window_size):
+        X, y = [], []
+        for i in range(len(data) - window_size - 1):
+            window = data[i:i+window_size]
+            X.append(window)
+            y.append(1 if data[i+window_size] > self.generate_ai_indicator(window) else -1)
+        return np.array(X), np.array(y)
 
-        # Log trade details
-        trade_price = order['price']
-        trade_quantity = order['amount']
-        trade_side = order['side']
-        trade_type = order['type']
-        logger.info(f"Trade Details: Price: {trade_price}, Quantity: {trade_quantity}, Side: {trade_side}, Type: {trade_type}")
+    def train(self, X, y):
+        self.classifier.fit(X, y)
 
-        return order
-    except Exception as e:
-        logger.error(f"Error placing order: {e}")
-        return None
+    def predict(self, data):
+        return self.classifier.predict([data])[0]
 
-# Define function to get account balance in USDT
-def get_account_balance():
-    try:
-        balance = bybit_exchange.fetch_balance()
-        return balance['USDT']['free']
-    except Exception as e:
-        logger.error(f"Error fetching account balance: {e}")
-        return 0.0
+class OrderPlacer:
+    def __init__(self, exchange):
+        self.exchange = exchange
 
-# Fetch real-time data for a specific trading pair and timeframe
-symbol = 'BTC/USDT'  # Use the perpetual BTC/USDT contract
-timeframe = '1m'  # Use a lower timeframe for real-time streaming, '1m' is just an example
-window_size = 10
+    def place_market_order(self, symbol, side, quantity, leverage):
+        try:
+            order = self.exchange.create_market_order(symbol, side, quantity, {'leverage': leverage})
 
-# Define the leverage for your trades
-leverage = 50  # Replace with your desired leverage
+            # Log trade details
+            trade_price = order['price']
+            trade_quantity = order['amount']
+            trade_side = order['side']
+            trade_type = order['type']
+            logger.info(f"Trade Details: Price: {trade_price}, Quantity: {trade_quantity}, Side: {trade_side}, Type: {trade_type}")
 
-# Define the desired risk percentage (e.g., 2%)
-risk_percentage = 2
+            return order
+        except Exception as e:
+            logger.error(f"Error placing order: {e}")
+            return None
 
-# Initialize the Random Forest Classifier
-classifier = RandomForestClassifier()
+    def get_account_balance(self):
+        try:
+            balance = self.exchange.fetch_balance()
+            return balance['USDT']['free']
+        except Exception as e:
+            logger.error(f"Error fetching account balance: {e}")
+            return 0.0
 
-# Main trading loop
-while True:
-    try:
-        # Fetch real-time data
-        real_time_data = fetch_real_time_data(symbol, timeframe)
+def main():
+    # Fetch real-time data for a specific trading pair and timeframe
+    symbol = 'BTC/USDT'  # Use the perpetual BTC/USDT contract
+    timeframe = '1m'  # Use a lower timeframe for real-time streaming, '1m' is just an example
+    window_size = 10
 
-        if real_time_data is not None:
-            # Generate AI indicator on real-time data
-            ai_indicator = generate_ai_indicator(real_time_data)
+    # Define the leverage for your trades
+    leverage = 50  # Replace with your desired leverage
 
-            # Create machine learning dataset
-            X, y = create_dataset(real_time_data, window_size)
+    # Define the desired risk percentage (e.g., 2%)
+    risk_percentage = 2
 
-            # Train the classifier
-            classifier.fit(X, y)
+    data_fetcher = DataFetcher(bybit_exchange)
+    indicator_generator = IndicatorGenerator()
+    ml_model = MachineLearningModel()
+    order_placer = OrderPlacer(bybit_exchange)
 
-            # Make prediction for the next data point
-            next_data_point = real_time_data[-window_size:]
-            prediction = classifier.predict([next_data_point])[0]
+    # Main trading loop
+    while True:
+        try:
+            # Fetch real-time data
+            real_time_data = data_fetcher.fetch_real_time_data(symbol, timeframe)
 
-            # Define trading signal based on the prediction
-            signal = 'BUY' if prediction == 1 else 'SELL'
+            if real_time_data is not None:
+                # Generate AI indicator on real-time data
+                ai_indicator = indicator_generator.generate_ai_indicator(real_time_data)
 
-            # Get account balance in USDT
-            account_balance = get_account_balance()
+                # Create machine learning dataset
+                X, y = ml_model.create_dataset(real_time_data, window_size)
 
-            # Calculate the trade amount in USDT based on available balance and risk percentage
-            risk_amount = (account_balance * risk_percentage) / 100
-            trade_amount = min(risk_amount, account_balance)  # Limit trade amount to available balance
+                # Train the classifier
+                ml_model.train(X, y)
 
-            # Log trading signal and trade amount
-            logger.info(f"Signal: {signal}, Trade Amount (USDT): {trade_amount:.2f}")
+                # Make prediction for the next data point
+                next_data_point = real_time_data[-window_size:]
+                prediction = ml_model.predict(next_data_point)
 
-            # Execute the trade
-            if signal == 'BUY':
-                place_market_order(symbol, 'buy', trade_amount, leverage)
-            elif signal == 'SELL':
-                place_market_order(symbol, 'sell', trade_amount, leverage)
+                # Define trading signal based on the prediction
+                signal = 'BUY' if prediction == 1 else 'SELL'
 
-        # Add a sleep interval to control the trading frequency (e.g., sleep for 1 minute)
-        time.sleep(60)
+                # Get account balance in USDT
+                account_balance = order_placer.get_account_balance()
 
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
+                # Calculate the trade amount in USDT based on available balance and risk percentage
+                risk_amount = (account_balance * risk_percentage) / 100
+                trade_amount = min(risk_amount, account_balance)  # Limit trade amount to available balance
+
+                # Log trading signal and trade amount
+                logger.info(f"Signal: {signal}, Trade Amount (USDT): {trade_amount:.2f}")
+
+                # Execute the trade
+                if signal == 'BUY':
+                    order_placer.place_market_order(symbol, 'buy', trade_amount, leverage)
+                elif signal == 'SELL':
+                    order_placer.place_market_order(symbol, 'sell', trade_amount, leverage)
+
+            # Add a sleep interval to control the trading frequency (e.g., sleep for 1 minute)
+            time.sleep(60)
+
+        except Exception as e:
+            logger.error(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
